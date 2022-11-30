24bdf982db17465d03a5d5d74e85dbabfd54781f
niccc69 <84114541+niccc69@users.noreply.github.com>
2022-11-30 11:03:35 -0500

    Update login-page.js

--- static/login-page.js
+++ static/login-page.js
@@ -11,7 +11,7 @@ loginButton.addEventListener("click", (e) => {
 
     if (username === "modz" && password === "cheeks") {
         alert("You have successfully logged in.");
-        window.location.replace="https://privateincogv1.herokuapp.com/verified.html"
+        window.location.assign("https://privateincogv1.herokuapp.com/verified.html")
     } 
     
     else {
